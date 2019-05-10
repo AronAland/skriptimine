b44a8d05e60df18e980826fc996119fad08bbf65
user <user@scriptaron>
2019-05-10 05:39:58 -0400

    vastus ühel real

--- praks5/yl1.sh
+++ praks5/yl1.sh
@@ -8,8 +8,7 @@ read kolmnurk_a
 echo -n "Sisesta b külje väärtus:"
 read kolmnurk_b
 
-aruudus=$(echo "scale=2;($kolmnurk_a^2)" | bc) # a külje ruut
-bruudus=$(echo "scale=2;($kolmnurk_b^2)" | bc) # b külje ruut
-summa=$(echo "scale=2;($aruudus+$bruudus)" | bc) # a ja b summa
-c=$(echo "scale=2;sqrt ($summa)" | bc) #vastus
+c=$(echo "scale=2;sqrt ($kolmnurk_a^2+$kolmnurk_b^2)" | bc) #vastus
+
+
 echo "c: $c" #vastus
