28168e34764461ba75240cce75a082541d88ecc0
AronAland <aron.aland@khk.ee>
2019-05-15 04:06:13 -0400

    aja järgi tervitamine

---
+++ praks7/yl3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#skript, mis tervitab kasutajat vastavalt sisestatud kellajale
+
+echo -n "Sisesta aja väärtus:"
+read aeg
+
+if [ $aeg -ge 6 -a $aeg -lt 12 ]; then
+ echo "Tere hommikust!"
+elif [ $aeg -ge 12 -a $aeg -lt 18 ]; then
+ echo "Tere päevast!"
+elif [ $aeg -ge 18 -a $aeg -lt 22 ]; then
+ echo "Tere õhtust!"
+elif [ $aeg -le 23 -a $aeg -ge 0 ]; then
+ echo "Head ööd!"
+else 
+ echo "[VIGA]: SA PEAD SISESTAMA AJA TUNDIDES!"
+fi
