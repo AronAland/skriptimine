31e1f375b49c3caf7971b2e02bdd282e1ede09de
AronAland <aron.aland@khk.ee>
2019-05-27 03:38:38 -0400

    täiendus

--- praks9/yl1.sh
+++ praks9/yl1.sh
@@ -1,18 +1,13 @@
 #/bin/bash
 #skript, mis väljastab kujundi
 
-rida=0
 
 for (( j = 1; j <= 5; j++))
 do
-for (( i = 1; i <= 5; i++))
-do
-  if [ $i -eq 1 ]
-  then
-   ((rida++))
-   echo -n "$rida."
-  fi
+ echo -n "$j."
+ for (( i = 1; i <= 5; i++))
+ do
   echo -n "*"
-done
-echo ""
+ done
+ echo ""
 done
